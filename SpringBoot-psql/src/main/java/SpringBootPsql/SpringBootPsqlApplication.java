8d84f38b2d5da4e0c5aee2ebf27742c6250e4ce5
nikhi <nikhi@LAPTOP-FPMEKVQP>
2020-06-20 11:34:46 +0530

    Task 4,5 and 6

---
+++ SpringBoot-psql/src/main/java/SpringBootPsql/SpringBootPsqlApplication.java
@@ -0,0 +1,13 @@
+package SpringBootPsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPsqlApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-psql/src/test/java/SpringBootPsql/SpringBootPsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootPsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
